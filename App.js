72c273cc9a4f54d386d59d8eb5c864c84c236b4d
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-11-26 18:45:51 +0200

    changes

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isLoading ? 
+      <ActivityIndicator size='large' animating={true}></ActivityIndicator>
+      : <Text>Page loaded</Text>}
     </View>
   );
 }
